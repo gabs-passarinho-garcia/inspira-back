b4ddc9f4ae06c8634bb0b95e31724b3c08b8fb7d
Gabriel de Freitas Garcia <gabrieldefreitasgarcia@outlook.com>
2024-10-26 00:05:14 -0300

    feat: created database

---
+++ prisma/migrations/20241026014343_content/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Content" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Content_pkey" PRIMARY KEY ("id")
+);
